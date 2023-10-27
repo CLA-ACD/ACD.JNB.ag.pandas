ec5debffa167c17f936f1cfce45443f4676cecb1
Ricardo Rafael Quintero Meza, PhD <84739791+ricardo-qm@users.noreply.github.com>
2023-10-27 11:12:58 -0700

    Initial commit

---
+++ pythonfiles/tests/test_Occupation.py
@@ -0,0 +1,31 @@
+from pythonfiles.files import Occupation
+
+def test_problema_01():
+    assert Occupation.r1
+
+def test_problema_02():
+    assert Occupation.r2
+    
+def test_problema_03():
+    assert Occupation.r3
+
+def test_problema_04():
+    assert Occupation.r4
+
+def test_problema_05():
+    assert Occupation.r5
+
+def test_problema_06():
+    assert Occupation.r6
+    
+def test_problema_07():
+    assert Occupation.r7
+
+def test_problema_08():
+    assert Occupation.r8
+
+def test_problema_09():
+    assert Occupation.r9
+
+def test_problema_10():
+    assert Occupation.r10
